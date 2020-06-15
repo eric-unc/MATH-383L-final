3ec5f06a5f2585a588ef6e5dc89a402e724f00be
Eric Schneider <u1643364@ad.unc.edu>
2020-06-14 23:34:01 -0400

    Finish prediction examples. I think I won't have to add anything more

--- Main.m
+++ Main.m
@@ -5,7 +5,8 @@
 figure()
 T_data = readmatrix('data.csv', 'Range', 'B:B');
 
-% This will convert the data from Fahrenheit to Kelvin
+% This will convert the data from Fahrenheit to Kelvin. It's a bit more
+% exact than the third column.
 T_data = ((T_data - 32).* (5/9)) + 273.15;
 
 t_data = 1:1:size(T_data);
@@ -36,7 +37,7 @@ t0 = 0;
 tf = 299;
 h = 1;
 
-[t1, y1] = RungeKutta4(@(t, T) diffeq(t, T), y0, t0, tf, round(tf / h));
+[t1, y1] = RungeKutta4(@(t, T) diffeq(t, T, 298), y0, t0, tf, round(tf / h));
 plot(t1, y1, 'm-*')
 hold on;
 
@@ -44,3 +45,25 @@ title('Solution v. Runge-Kutta (order 4)')
 xlabel('Time (minutes)')
 ylabel('Temperature (K)')
 legend('Solution', 'Runge-Kutta (h = 1)')
+
+%% Using a lower initial temperature in a warmer room
+figure();
+[t2, y2] = RungeKutta4(@(t, T) diffeq(t, T, 305.37), 273.71, t0, tf, round(tf / h));
+plot(t2, y2, 'm-*')
+hold on;
+
+title('Runge-Kutta (order 4) with 274 K initial and 305 ambient')
+xlabel('Time (minutes)')
+ylabel('Temperature (K)')
+legend('Runge-Kutta (h = 1)')
+
+%% Using a higher initial temperature
+figure();
+[t2, y2] = RungeKutta4(@(t, T) diffeq(t, T, 298), 338.71, t0, tf, round(tf / h));
+plot(t2, y2, 'm-*')
+hold on;
+
+title('Runge-Kutta (order 4) with 339 K initial and 298 K ambient')
+xlabel('Time (minutes)')
+ylabel('Temperature (K)')
+legend('Runge-Kutta (h = 1)')
--- diffeq.m
+++ diffeq.m
@@ -1,3 +1,3 @@
-function dy = diffeq(t, T)
-    dy = -0.0187*(T - 298);
+function dy = diffeq(t, T, Ta)
+    dy = -0.0187*(T - Ta);
 end
